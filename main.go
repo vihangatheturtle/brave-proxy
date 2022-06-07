1073bbe83bd3d006c940e4d3606c8cf1388fc75f
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-06-07 22:33:24 +0100

    added maintenance endpoints

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 
 var soft_version string
 var blockedDomains []string
+var isUnderMaintenance bool
 
 func main() {
 	// Set initial variables
 	soft_version = "0.0.1"
+	isUnderMaintenance = false
 	blockedDomains = []string{"t1777799.com", "www.t1777799.com", "www.797ka.cn", "797ka.cn", "s1ndwdrld.logicdn.com"}
 
 	log.Println("Starting brave proxy server on port 443")
@@ -37,6 +39,50 @@ func main() {
 					200,
 					"Connected to Brave Proxy by Cosmos Softwares running on version "+soft_version,
 				)
+			} else if r.Host == "brave-node-maintenance.cosmos-softwares.com" {
+				if isUnderMaintenance {
+					return r, goproxy.NewResponse(r,
+						goproxy.ContentTypeText,
+						400,
+						"This node is under maintenance",
+					)
+				} else {
+					return r, goproxy.NewResponse(r,
+						goproxy.ContentTypeText,
+						200,
+						"This node is active",
+					)
+				}
+			} else if r.Host == "brave.node.maintenance.activate.pI0Hn26UkvVX" {
+				if isUnderMaintenance {
+					return r, goproxy.NewResponse(r,
+						goproxy.ContentTypeText,
+						400,
+						"This node is already under maintenance",
+					)
+				} else {
+					isUnderMaintenance = true
+					return r, goproxy.NewResponse(r,
+						goproxy.ContentTypeText,
+						200,
+						"This node is now under maintenance",
+					)
+				}
+			} else if r.Host == "brave.node.maintenance.activate.1KtcVMy70uXJ" {
+				if !isUnderMaintenance {
+					return r, goproxy.NewResponse(r,
+						goproxy.ContentTypeText,
+						400,
+						"This node is not under maintenance",
+					)
+				} else {
+					isUnderMaintenance = false
+					return r, goproxy.NewResponse(r,
+						goproxy.ContentTypeText,
+						200,
+						"This node is no longer under maintenance",
+					)
+				}
 			}
 			return r, nil
 		})
